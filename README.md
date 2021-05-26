58c8f3cdbb9d12c345971898e855c74a45c133b5
Daniel Cousens <dcousens@users.noreply.github.com>
2021-05-26 14:30:46 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ?
+A `node_modules` license-file inspection tool for monorepos that uses yarn workspaces.
+
+**WARNING**: This tool is a work in progress, please report any bugs! If you need something safe for your production tooling, this probably isn't for you... yet.
+
+## Examples
+```
+```
+
+## LICENSE [MIT](LICENSE)
+Copyright (c) 2021 Thinkmill Labs Pty Ltd
---
+++ index.mjs
@@ -0,0 +1,99 @@
+//  MIT License
+//
+//  Copyright (c) 2021 Thinkmill Labs Pty Ltd
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+import fs from 'fs'
+
+const { argv } = process
+
+function parseList (prefix) {
+  const list = []
+  for (const arg of argv) {
+    if (!arg.startsWith(prefix)) continue
+    list.push(...arg.slice(prefix.length).split(','))
+  }
+  return list
+}
+
+const printCounts = argv.includes('--counts')
+const printCsv = argv.includes('--csv')
+const approved = new Set(parseList('--approved=')) // list of approved SPDX identifiers
+const excludes = new Set(parseList('--excludes=')) // list of excluded package names
+const excludePrefixes = parseList('--excludePrefix=') // list of exclude package prefixes
+
+const stdin = fs.readFileSync(0)
+const { head, body } = JSON.parse(stdin.toString('utf8'))?.data
+
+function fail (message) {
+  console.error(message)
+  process.exit(1)
+}
+
+if (printCsv) {
+  console.log(`Name, SPDX, Description, Author/Contributors, URLs`)
+}
+
+const counts = {}
+for (const rowArray of body) {
+  const row = {}
+  head.forEach((key, i) => (row[key] = rowArray[i]))
+
+  const {
+    Name: name,
+    License: spdx
+  } = row
+
+  // skip excluded packages
+  if (excludes.has(name)) continue
+
+  // skip if prefix matches
+  if (excludePrefixes.some((prefix) => {
+    return name.startsWith(prefix)
+  })) continue
+
+  // exit error code if unapproved
+  if (approved.length && !approved.has(spdx)) {
+    fail(`Unapproved license ${license}`)
+    break
+  }
+
+  if (printCsv) {
+    const packageJson = JSON.parse(fs.readFileSync(`./node_modules/${name}/package.json`))
+    const {
+      description = '',
+      author = '',
+      contributors = [],
+
+      homepage,
+      repository
+    } = packageJson
+
+    const authorName = author?.name || author
+    const contributorNames = contributors.map(x => x?.name || x)
+    const everyone = [authorName, ...contributorNames].join(',')
+    const urls = [homepage, repository?.url || repository].filter(Boolean).join(',')
+
+    console.log(`${name}, ${spdx}, ${description}, "${everyone}", "${urls}"`)
+  }
+}
+
+if (printCounts) {
+  console.log(counts)
+}
